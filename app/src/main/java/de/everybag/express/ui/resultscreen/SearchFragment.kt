104075eeb0ecf1ea5faf9d20ffabd1ee87256918
Sidali Mellouk <s.mellouk@nyris.io>
2018-06-19 16:45:18 +0200

    Update search fragment UI

--- app/src/main/java/de/everybag/express/ui/resultscreen/SearchFragment.kt
+++ app/src/main/java/de/everybag/express/ui/resultscreen/SearchFragment.kt
@@ -94,10 +94,6 @@ class SearchFragment @Inject constructor() : BaseFragment<SearchResultContract.P
         if (listOffers == null) {
             mPresenter.searchOffers(image)
         } else {
-            if (listOffers.size == 0) {
-                showError("No offers found for the selected object")
-                return
-            }
             showOffers(listOffers)
             hideProgress()
         }
@@ -126,6 +122,10 @@ class SearchFragment @Inject constructor() : BaseFragment<SearchResultContract.P
     }
 
     override fun showOffers(offers: ArrayList<OfferParcelable>) {
+        if (offers.size == 0) {
+            showError("No offers found for the selected object")
+            return
+        }
         mOffersAdapter.setOffers(offers)
         showSnackViewOnce()
     }
