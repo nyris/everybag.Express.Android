9243f73fdfa38d70b604f807f8ac488b9ac500dc
Sidali Mellouk <s.mellouk@nyris.io>
2018-08-28 14:32:22 +0200

    Add new UX to offer not found

---
+++ app/src/main/java/de/everybag/express/ui/resultscreen/IOnBottomReachedListener.kt
@@ -0,0 +1,13 @@
+package de.everybag.express.ui.resultscreen
+
+/**
+ * IOnBottomReachedListener.kt - Listener use to check if RecycleView reached bottom
+ *
+ * @author Sidali Mellouk
+ * Created by nyris GmbH
+ * Copyright © 2018 nyris GmbH. All rights reserved.
+ */
+
+interface IOnBottomReachedListener{
+    fun onBottomReached()
+}
